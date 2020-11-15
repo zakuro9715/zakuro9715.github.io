b487570421ebcddb1f0c413350e08af13769d248
zakuro <z@kuro.red>
2020-11-16 01:11:49 +0900

    Update dotfiles install script

--- static/install.zsh
+++
@@ -1,6 +0,0 @@
-dotfiles="$HOME/src/github.com/zakuro9715/dotfiles"
-if [[ -n $dotfiles ]]
-then
-  git clone git@github.com:zakuro9715/dotfiles $dotfiles
-fi
-curl https://raw.githubusercontent.com/zakuro9716/dotfiles/master/install.zsh
